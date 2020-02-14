a0379a8c05e9e93f279e780ccc4cbff009112146
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-02-14 15:39:46 +0300

    feat: add basic redux

---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import {changeFirstName, changeSecondName} from '../store/actions';
+
+class MainComponent extends React.Component {
+
+    render() {
+        console.log(this.props);
+        const { firstName, secondName, changeFirstName, changeSecondName } = this.props;
+        return (<div>
+            <div>
+                <input
+                    type='text'
+                    value={this.props.firstName}
+                    plaseholder="firstName"
+                    onChange={(event) => {
+                        changeFirstName(event.target.value)
+                    }}
+                >
+                </input>
+            </div>
+            <div>
+                <input
+                    type='text'
+                    value={this.props.secondName}
+                    plaseholder="secondName"
+                    onChange={(event) => {
+                        changeSecondName(event.target.value)
+                    }}
+                >
+                </input>
+            </div>
+            <div>
+                {firstName} {secondName}
+            </div>
+        </div>)
+    }
+}
+
+const mapStateToProps = (state) => {
+    // console.log(state);
+    return {
+        firstName: state.firstName,
+        secondName: state.secondName,
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        changeFirstName: bindActionCreators(changeFirstName, dispatch),
+        changeSecondName: bindActionCreators(changeSecondName, dispatch),
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainComponent);
---
+++ src/store/actions.js
@@ -0,0 +1,18 @@
+import { ACTION_CHANGE_FIRST_NAME, ACTION_CHANGE_SECOND_NAME } from '../index';
+
+
+const changeFirstName = (newFirstName) => {
+    return {
+        type: ACTION_CHANGE_FIRST_NAME,
+        payload: newFirstName,
+    };
+};
+
+const changeSecondName = (secondFirstName) => {
+    return {
+        type: ACTION_CHANGE_SECOND_NAME,
+        payload: secondFirstName,
+    };
+};
+
+export {changeFirstName, changeSecondName};
